0e6520088f83c91d583b290bbaa503a0e67730df
m-tonon <math.tonon@gmail.com>
2023-06-09 06:44:21 -0300

    feat: creating interfaces for flights

--- ClientApp/src/app/search-flights/search-flights.component.ts
+++ ClientApp/src/app/search-flights/search-flights.component.ts
@@ -2,6 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavMenuComponent } from '../nav-menu/nav-menu.component';
 
+export interface FlightRm {
+  airline: string;
+  arrival: TimePlaceRm;
+  departure: TimePlaceRm;
+  reaminingSeats: number;
+  price: string;
+}
+
+export interface TimePlaceRm {
+  place: string;
+  time: string;
+}
+
 @Component({
   selector: 'app-search-flights',
   standalone: true,
@@ -14,9 +27,27 @@ import { NavMenuComponent } from '../nav-menu/nav-menu.component';
 })
 export class SearchFlightsComponent {
   
-  searchResult: any = [
-    "American Airlines",
-    "British Airways",
-    "Lufthansa"
+  searchResult: FlightRm[] = [
+    {
+      airline: "American Airlines",
+      departure: { place: "Los Angeles", time: Date.now().toString() },
+      arrival: { place: "Istambul", time: Date.now().toString() },
+      reaminingSeats: 500,
+      price: "500"
+    },
+    {
+      airline: "Deutsche BA",
+      departure: { place: "Munchen", time: Date.now().toString() },
+      arrival: { place: "Schiphol", time: Date.now().toString() },
+      reaminingSeats: 60,
+      price: "600"
+    },
+    {
+      airline: "British Airways",
+      departure: { place: "London, England", time: Date.now().toString() },
+      arrival: { place: "Vizzola-Ticino", time: Date.now().toString() },
+      reaminingSeats: 60,
+      price: "600"
+    },
   ];
 }

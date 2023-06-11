fc5a1fdb2728e45f3b3bc9e8fc089aaa2d2b4b09
m-tonon <math.tonon@gmail.com>
2023-06-11 08:45:10 -0300

    feat: set handleerror function

--- ClientApp/src/app/search-flights/search-flights.component.ts
+++ ClientApp/src/app/search-flights/search-flights.component.ts
@@ -8,21 +8,23 @@ import { FlightService } from 'src/api/services';
 @Component({
   selector: 'app-search-flights',
   standalone: true,
-  imports: [
-    CommonModule,
-    NavMenuComponent
-  ],
+  imports: [CommonModule, NavMenuComponent],
   templateUrl: './search-flights.component.html',
-  styleUrls: ['./search-flights.component.css']
+  styleUrls: ['./search-flights.component.css'],
 })
 export class SearchFlightsComponent {
-  
   searchResult: FlightRm[] = [];
 
-  constructor(private flightService: FlightService ) { }
+  constructor(private flightService: FlightService) {}
 
   search() {
-    this.flightService.flightGet({})
-      .subscribe(response => this.searchResult = response);
+    this.flightService.flightGet({}).subscribe({
+      next: (response: any) => (this.searchResult = response),
+      error: this.handleError,
+    });
+  }
+
+  private handleError(error: any) {
+    console.error(error);
   }
 }

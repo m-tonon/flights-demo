b67670ae3de789aab3f838936f2d3e7508a0a7cf
m-tonon <math.tonon@gmail.com>
2023-06-11 08:36:21 -0300

    feat: import httpclient service to the providers

--- ClientApp/src/app/app.config.ts
+++ ClientApp/src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 import { SearchFlightsComponent } from './search-flights/search-flights.component';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
+    provideHttpClient(),
     provideRouter([
       { path: '', component: SearchFlightsComponent, pathMatch: 'full' },
     ]),

ac6a0ea6879609893dd41c8d199727683aec5eff
m-tonon <math.tonon@gmail.com>
2023-06-24 07:27:42 -0300

    feat: seed data in program.cs

--- Data/Entities.cs
+++ Data/Entities.cs
@@ -6,41 +6,7 @@ namespace Flights.Data
     {
         public IList<Passenger> Passengers = new List<Passenger>();
 
-        static Random random = new Random();
+        public List<Flight> Flights = new List<Flight>();
 
-        public Flight[] Flights = new Flight[]
-        {
-        new ( Guid.NewGuid(),
-              "American Airlines",
-              random.Next(90, 5000).ToString(),
-              new TimePlace("Los Angeles", DateTime.Now.AddHours(random.Next(1,3))),
-              new TimePlace("Istambul", DateTime.Now.AddHours(random.Next(4,10))),
-              2),
-        new ( Guid.NewGuid(),
-              "Deutsche BA",
-              random.Next(90, 5000).ToString(),
-              new TimePlace("Munchen", DateTime.Now.AddHours(random.Next(1,10))),
-              new TimePlace("Schipol", DateTime.Now.AddHours(random.Next(4,15))),
-              random.Next(1, 853)),
-        new ( Guid.NewGuid(),
-              "British Airways",
-              random.Next(90, 5000).ToString(),
-              new TimePlace("London England", DateTime.Now.AddHours(random.Next(1,15))),
-              new TimePlace("Vizzola-Ticino", DateTime.Now.AddHours(random.Next(4,18))),
-              random.Next(1, 853)),
-        new ( Guid.NewGuid(),
-              "Air France",
-              random.Next(90, 5000).ToString(),
-              new TimePlace("Paris", DateTime.Now.AddHours(random.Next(1,20))),
-              new TimePlace("Munchen", DateTime.Now.AddHours(random.Next(4,25))),
-              random.Next(1, 853)),
-        new ( Guid.NewGuid(),
-              "KLM",
-              random.Next(90, 5000).ToString(),
-              new TimePlace("Schipol", DateTime.Now.AddHours(random.Next(1,30))),
-              new TimePlace("London England", DateTime.Now.AddHours(random.Next(4,35))),
-              random.Next(1, 853)),
-        };
-
-  }
-}  
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.OpenApi.Models;
 using Flights.Data;
+using Flights.Domain.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,46 @@ builder.Services.AddSwaggerGen(c =>
 builder.Services.AddSingleton<Entities>();
 
 var app = builder.Build();
+
+// this will be used to seed the database
+var entities = app.Services.CreateScope().ServiceProvider.GetService<Entities>();
+var random = new Random();
+
+Flight[] flightsToSeed = new Flight[]
+{
+  new ( Guid.NewGuid(),
+              "American Airlines",
+              random.Next(90, 5000).ToString(),
+              new TimePlace("Los Angeles", DateTime.Now.AddHours(random.Next(1,3))),
+              new TimePlace("Istambul", DateTime.Now.AddHours(random.Next(4,10))),
+              2),
+        new ( Guid.NewGuid(),
+              "Deutsche BA",
+              random.Next(90, 5000).ToString(),
+              new TimePlace("Munchen", DateTime.Now.AddHours(random.Next(1,10))),
+              new TimePlace("Schipol", DateTime.Now.AddHours(random.Next(4,15))),
+              random.Next(1, 853)),
+        new ( Guid.NewGuid(),
+              "British Airways",
+              random.Next(90, 5000).ToString(),
+              new TimePlace("London England", DateTime.Now.AddHours(random.Next(1,15))),
+              new TimePlace("Vizzola-Ticino", DateTime.Now.AddHours(random.Next(4,18))),
+              random.Next(1, 853)),
+        new ( Guid.NewGuid(),
+              "Air France",
+              random.Next(90, 5000).ToString(),
+              new TimePlace("Paris", DateTime.Now.AddHours(random.Next(1,20))),
+              new TimePlace("Munchen", DateTime.Now.AddHours(random.Next(4,25))),
+              random.Next(1, 853)),
+        new ( Guid.NewGuid(),
+              "KLM",
+              random.Next(90, 5000).ToString(),
+              new TimePlace("Schipol", DateTime.Now.AddHours(random.Next(1,30))),
+              new TimePlace("London England", DateTime.Now.AddHours(random.Next(4,35))),
+              random.Next(1, 853)),
+};
+entities.Flights.AddRange(flightsToSeed);
+
 
 app.UseCors(builder => builder
   .WithOrigins("*")

3a1cea5fb1910b58e4fa82caf09a2b67cc7ab58c
m-tonon <math.tonon@gmail.com>
2023-06-25 07:31:53 -0300

    feat: set primary key for Passenger entity and configuring Departure and Arrival to be stored within Flight entity

--- Data/Entities.cs
+++ Data/Entities.cs
@@ -7,10 +7,17 @@ namespace Flights.Data
     {
         public DbSet<Passenger> Passengers => Set<Passenger>();
         public DbSet<Flight> Flights => Set<Flight>();
-    }
 
-    public Entities(DbContextOptions<Entities> options) : base(options)
-    {
+        public Entities(DbContextOptions<Entities> options) : base(options)
+        {
+        }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Passenger>().HasKey(p => p.Email);
+
+            modelBuilder.Entity<Flight>().OwnsOne(f => f.Departure);
+            modelBuilder.Entity<Flight>().OwnsOne(f => f.Arrival);
+        }
     }
 }

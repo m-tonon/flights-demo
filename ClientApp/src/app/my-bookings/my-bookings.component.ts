c02ee0a36d538382d16015722e75c8151ffcb204
m-tonon <math.tonon@gmail.com>
2023-06-25 09:08:42 -0300

    feat: redirect if user isnt logged in

--- ClientApp/src/app/my-bookings/my-bookings.component.ts
+++ ClientApp/src/app/my-bookings/my-bookings.component.ts
@@ -4,29 +4,35 @@ import { CommonModule } from '@angular/common';
 import { BookingRm } from 'src/api/models/booking-rm';
 import { BookingService } from 'src/api/services';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-bookings',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './my-bookings.component.html',
-  styleUrls: ['./my-bookings.component.css']
+  styleUrls: ['./my-bookings.component.css'],
 })
 export class MyBookingsComponent implements OnInit {
   bookings!: BookingRm[];
-  
+
   constructor(
     private bookingService: BookingService,
-    private authService: AuthService) {}
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    if (!this.authService.currentUser?.email) {
+      this.router.navigate(['/register-passenger']);
+    }
+
     const _email = this.authService.currentUser?.email ?? '';
 
-    this.bookingService.listBooking({email: _email})
-    .subscribe({
-      next: r => this.bookings = r,
-      error: this.handleError
-    })
+    this.bookingService.listBooking({ email: _email }).subscribe({
+      next: (r) => (this.bookings = r),
+      error: this.handleError,
+    });
   }
 
   private handleError(error: any) {

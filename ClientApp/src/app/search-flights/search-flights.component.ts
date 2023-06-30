5bb6003195bde85e9909358cfcc637f0bb743733
m-tonon <math.tonon@gmail.com>
2023-06-30 06:13:51 -0300

    feat: search flight form binding

--- ClientApp/src/app/search-flights/search-flights.component.ts
+++ ClientApp/src/app/search-flights/search-flights.component.ts
@@ -1,22 +1,33 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+
 import { NavMenuComponent } from '../nav-menu/nav-menu.component';
 import { FlightRm } from 'src/api/models';
 import { TimePlaceRm } from 'src/api/models';
 import { FlightService } from 'src/api/services';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-search-flights',
   standalone: true,
-  imports: [CommonModule, RouterModule, NavMenuComponent],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule ,NavMenuComponent],
   templateUrl: './search-flights.component.html',
   styleUrls: ['./search-flights.component.css'],
 })
 export class SearchFlightsComponent {
   searchResult: FlightRm[] = [];
+  searchForm = this.fb.group({
+    from: [''],
+    destination: [''],
+    fromData: [''],
+    toDate: [''],
+    numberOfPassengers: [1]
+  });
 
-  constructor(private flightService: FlightService) {}
+  constructor(
+    private flightService: FlightService,
+    private fb: FormBuilder) {}
 
   search() {
     this.flightService.searchFlight({}).subscribe({

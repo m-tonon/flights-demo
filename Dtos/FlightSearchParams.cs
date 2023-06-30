58f704bc0dc5590f7281437f274a8cfb3b9e94c7
m-tonon <math.tonon@gmail.com>
2023-06-30 06:28:25 -0300

    feat: add flight search params dto

---
+++ Dtos/FlightSearchParams.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel;
+
+namespace Flights.Dtos
+{
+    public record FlightSearchParams(
+
+      [DefaultValue("12/25/2023 10:30:00 AM")]
+      DateTime? FromDate,
+
+      [DefaultValue("12/26/2023 10:30:00 AM")]
+      DateTime? ToDate,
+
+      [DefaultValue("Los Angeles")]
+      string? From,
+
+      [DefaultValue("Berlin")]
+      string? Destination,
+      
+      [DefaultValue(1)]
+      int? NumberOfPassengers
+    );
+}
